8ab4bc6febb2c3e5b0b64ec242d63c656027d760
Elias Flores <eliasflores13@outlook.com>
2018-09-25 13:43:03 -0400

    Integrated the youtube search API and fetched some initial videos to the app

--- src/api/youtubeAPIKey.js
+++ src/api/youtubeAPIKey.js
@@ -1,1 +1,4 @@
 //Place the Youtube Search API Key here
+const API_KEY = 'AIzaSyDxC_6l_H_HpMcxP3SEn-4EJzyOYs7g46I';
+
+export default API_KEY;
--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './SearchBar';
 
+import API_KEY from '../api/youtubeAPIKey';
+
 class App extends Component {
+  state = {
+    videos: [],
+  }
+
+  componentDidMount() {
+    this.searchVideo('kobe bryant');
+  }
+
+  searchVideo = (term) => {
+    YTSearch({
+      key: API_KEY,
+      term: term
+    }, (videos) => {
+      this.setState({
+        videos: videos,
+      })
+    });
+  }
+
   render() {
     return (
       <div>
