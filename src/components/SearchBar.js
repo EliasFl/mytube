ba8692d642d71a950acbcc815d55a0fdfe2642c3
Elias Flores <eliasflores13@outlook.com>
2018-09-25 13:34:25 -0400

    Created the SearchBar component and integrated to the page

---
+++ src/components/SearchBar.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  state = {
+    term: '',
+  }
+
+  handleChange = (event) => {
+    this.changeTerm(event.target.value);
+  }
+
+  changeTerm = (newTerm) => {
+    this.setState({
+      term: newTerm,
+    });
+  }
+
+  render() {
+    return (
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
